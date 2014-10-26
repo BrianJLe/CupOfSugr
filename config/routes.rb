c13225b4ab16a464fa085da6c17ecd74f24a3480
brianlethal <litoblee@gmail.com>
2014-10-26 19:08:31 -0400

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Reservester::Application.routes.draw do
-  get "restaurants/show"
-  get "restaurants/index"
-  get "restaurants/new"
-  get "restaurants/create"
-  get "restaurants/edit"
-  get "restaurants/update"
-  get "restaurants/destroy"
 
    resources :restaurants
 
